a6b66507d919225d46cb619a3f555e4bead702be
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2022-01-29 14:33:38 +0100

    ask for user and pss

--- titan.sh
+++ titan.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
-[ -z "$progsfile"] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 
 install_package() {
@@ -13,33 +12,77 @@ newperms() { # Set special sudoers settings for install (or after).
 	echo "$* #TITAN" >> /etc/sudoers ;
 }
 
+get_username_and_pass() {
+    printf "username: " >&2
+    read -r username
+    while ! echo "$username" | grep -q "^[a-z_][a-z0-9_-]*$"; do
+        printf "username invalid. must begin with letter, only lowercase, - or _.: " >&2
+        read -r username
+    done
+    printf "enter password: "
+    read -r pass1
+    printf "confirm: "
+    read -r pass2
+    while ! [ "$pass1" = "$pass2" ]; do
+        printf "passwords don't match!\n"
+        unset pass2
+        printf "enter password: "
+        read -r pass1
+        printf "confirm: "
+        read -r pass2
+    done
+}
+
+confirm() {
+    printf "PROCEED? [Y/n] "
+    read -r ok
+    echo "$ok" | grep -q "^[nN]" && exit 1
+}
+
+setup() {
+    # install necessary packages
+    for x in curl ca-certificates base-devel git ntp zsh ; do
+        install_package "$x"
+    done
+
+    useradd -m -g wheel -s /bin/zsh "$username"
+    repodir="/home/$username/.local/src"
+    mkdir -p "$repodir"
+    chown -R alberto:wheel "$(dirname "$repodir")"
+    echo "$username:$pass1" | chpasswd
+    unset pass1 pass2;
+
+    [ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
+
+    newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
+
+    # Make pacman colorful, concurrent downloads and Pacman eye-candy.
+    grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+    sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
+}
+
+install_yay() {
+    # Use all cores for compilation.
+    sed -i "s/-j2/-j$(nproc)/;s/^#MAKEFLAGS/MAKEFLAGS/" /etc/makepkg.conf
+    
+    sudo -u $username mkdir -p "$repodir/yay-bin"
+    sudo -u $username git clone --depth 1 "https://aur.archlinux.org/yay-bin.git" "$repodir/yay-bin"
+    cd "$repodir/yay-bin"
+    sudo -u $username -D "$repodir/yay-bin" makepkg --noconfirm -si
+}
+
 install_loop() {
     # fetch the file with the list of programs
     ([ -f "$progsfile" ] && cp "$progsfile" progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > progs.csv
 }
 
 
-for x in curl ca-certificates base-devel git ntp zsh ; do
-    install_package "$x"
-done
+get_username_and_pass
 
-useradd -m -g wheel -s /bin/zsh "alberto"
-repodir="/home/alberto/.local/src"
-mkdir -p "$repodir"
-chown -R alberto:wheel "$(dirname "$repodir")"
-echo "alberto:arch" | chpasswd
+confirm
 
-[ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
+setup
 
-newperms "%wheel ALL=(ALL) NOPASSWD: ALL"
-
-# Make pacman colorful, concurrent downloads and Pacman eye-candy.
-grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
-sed -i "s/^#ParallelDownloads = 8$/ParallelDownloads = 5/;s/^#Color$/Color/" /etc/pacman.conf
-
-sudo -u alberto mkdir -p "$repodir/yay-bin"
-sudo -u alberto git clone --depth 1 "https://aur.archlinux.org/yay-bin.git" "$repodir/yay-bin"
-cd "$repodir/yay-bin"
-sudo -u alberto -D "$repodir/yay-bin" makepkg --noconfirm -si
+install_yay
 
 install_loop
